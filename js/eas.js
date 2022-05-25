0617efc7d50be99d4fcdc82fee34c9e71b031793
Connor Smith <connorsmith2002@gmail.com>
2022-05-24 20:20:58 -0400

    added color picker

--- js/eas.js
+++ js/eas.js
@@ -1,59 +1,65 @@
-let rows = document.querySelectorAll(".row");
+let rows = document.querySelectorAll('.row');
 let click = false;
 let eraser = false;
+let color = '#FF0000';
 
 console.log(rows);
 rows.forEach(function (row) {
     for (i = 0; i < 16; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
+        let square = document.createElement('div');
+        square.classList.add('square');
         row.appendChild(square);
     }
 });
 
-document.addEventListener("mousedown", (event) => {
+document.addEventListener('mousedown', (event) => {
     click = true;
 });
 
-document.addEventListener("mouseup", (event) => {
+document.addEventListener('mouseup', (event) => {
     click = false;
 });
 
-let eraserBTN = document.querySelector("#eraser");
-eraserBTN.addEventListener("click", (event) => {
+let eraserBTN = document.querySelector('#eraser');
+eraserBTN.addEventListener('click', (event) => {
     eraser = true;
 });
 
-let penBTN = document.querySelector("#pen");
-penBTN.addEventListener("click", (event) => {
+let penBTN = document.querySelector('#pen');
+penBTN.addEventListener('click', (event) => {
     eraser = false;
 });
 
-let clearBTN = document.querySelector("#clear");
-clearBTN.addEventListener("click", (event) => {
-    let squares = document.querySelectorAll(".square");
+let clearBTN = document.querySelector('#clear');
+clearBTN.addEventListener('click', (event) => {
+    let squares = document.querySelectorAll('.square');
     squares.forEach(function (square) {
-        square.style.backgroundColor = "white";
+        square.style.backgroundColor = 'white';
     });
 });
 
 rows.forEach((row) => {
-    row.addEventListener("mouseover", (event) => {
+    row.addEventListener('mouseover', (event) => {
         let square = event.target;
         if (click) {
             if (!eraser) {
-                square.style.backgroundColor = "red";
+                square.style.backgroundColor = color;
             } else {
-                square.style.backgroundColor = "white";
+                square.style.backgroundColor = 'white';
             }
         }
     });
 
-    row.addEventListener("mousedown", (event) => {
+    row.addEventListener('mousedown', (event) => {
         if (!eraser) {
-            event.target.style.backgroundColor = "red";
+            event.target.style.backgroundColor = color;
         } else {
-            event.target.style.backgroundColor = "white";
+            event.target.style.backgroundColor = 'white';
         }
     });
+});
+
+colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener('change', (element) => {
+    color = element.target.value.toString();
 });
